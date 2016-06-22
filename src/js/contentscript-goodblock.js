2b78590e423ea5237b05abf8b3fa9adcee03d368
Kevin Jennison <kevin.jennison1@gmail.com>
2016-06-22 11:40:29 -0700

    Simplify Goodblock content script.

--- src/js/contentscript-goodblock.js
+++ src/js/contentscript-goodblock.js
@@ -4,52 +4,23 @@
 /******************************************************************************/
 
 var baseElemId = 'goodblock-iframe-base';
-var goodblockIframeId = 'goodblock-iframe';
 
 var createGbScript = function() {
 	var script = document.createElement('script');
 	script.id = baseElemId;
 	script.src = process.env.GOODBLOCK_SCRIPT_SRC;
 	script.async = 'async';
-	script.dataset.goodblockContentScriptNum = 0;
-	document.getElementsByTagName('head')[0].appendChild(script);
+	document.body.appendChild(script);
 	return script;
 }
 
-var destroyGbScript = function(scriptElement) {
-	if(scriptElement){
-		document.getElementsByTagName('head')[0].removeChild(scriptElement);
-	}
-}
-
-// Remove elements created by Goodblock.
-var destroyGbElems = function() {
-	var gbIframe = document.querySelector('#' + goodblockIframeId);
-	if(gbIframe){
-		document.getElementsByTagName('body')[0].removeChild(gbIframe);
-	}
-}
-
-// Create the Goodblock script, destroying the existing one if it
-// exists.
+// Create the Goodblock script if it does not exist.
+// Return the script element.
 var createGbScriptIdempotent = function() {
 	var baseElem = document.querySelector('#' + baseElemId);
-
-	// Destroy the existing elements.
-	if (baseElem) {
-		destroyGbScript(baseElem);
-		destroyGbElems();
+	if (!baseElem) {
+		baseElem = createGbScript();
 	}
-
-	baseElem = createGbScript();
-	return baseElem;
-}
-
-// Update the Goodblock app elements, creating them if they don't exist.
-var initGoodblock = function() {
-
-	// Make sure our base elem exists.
-	var baseElem = createGbScriptIdempotent();
 }
 
 // When this content script executes, there are two possibilities:
@@ -61,7 +32,7 @@ var initGoodblock = function() {
 //	     has run in the current page. This can happen when the extension
 //		 updates, when the user manually reloads the extension, or if
 //		 the user uninstalls and reinstalls without reloading a web page.
-initGoodblock();
+createGbScriptIdempotent();
 
 /******************************************************************************/
 /******************************************************************************/
